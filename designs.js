38eb320342ae4640df197aba1512ea360e22d764
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-01-11 22:42:04 +0100

    moved event listener to inner loop and worked on unset backgroundcolor

--- designs.js
+++ designs.js
@@ -1,37 +1,48 @@
 function makeGrid(event) {
+  // prevent the page from reloading
   event.preventDefault();
-  // getting access to size input values
-  let inputCols = document.getElementById("inputWidth").value;
+  // getting access to the table
+  let canvasTable = document.getElementById("pixelCanvas");
+  // getting access to the size input values
+  let inputCells = document.getElementById("inputWidth").value;
   let inputRows = document.getElementById("inputHeight").value;
-
   // when form is submitted all children get removed (if any)
   while (canvasTable.firstChild) {
     canvasTable.removeChild(canvasTable.firstChild);
   }
-
-  // for-loop for building rows and cols
+  // for-loop for building rows and cells
+  // adding an event listener to every cell
   for (let row = 0; row < inputRows; row++) {
     let rowElement = document.createElement("tr");
     canvasTable.appendChild(rowElement);
-    for (let col = 0; col < inputCols; col++) {
-      let colElement = document.createElement("td");
-      rowElement.appendChild(colElement);
+    for (let cell = 0; cell < inputCells; cell++) {
+      let cellElement = document.createElement("td");
+      rowElement.appendChild(cellElement);
+      cellElement.addEventListener("click", colorBackground);
     }
-  } // automatic return??
+  }
 }
 
-// select color input and set background color
+// select color input and set / unset background color
 function colorBackground(event) {
   event.preventDefault();
   let colorInput = document.getElementById("colorPicker").value;
-  event.target.style.backgroundColor = colorInput;
+  let currentCell = event.target;
+  // convert color input to string to make it comparable
+  function compareColor(color) {
+    let colorString = document.createElement("div");
+    colorString.style.backgroundColor = `${color}`;
+    return colorString.style.backgroundColor;
+  }
+
+  if (currentCell.style.backgroundColor === compareColor(colorInput)) {
+    currentCell.setAttribute("style", "background-color: white");
+  } else {
+    currentCell.style.backgroundColor = colorInput;
+  }
 }
 
 // select size input
-// when size is submitted by the user, call makeGrid()
-let canvasTable = document.getElementById("pixelCanvas");
+// and add event listeners
 let inputForm = document.getElementById("sizePicker");
-
-// add event listeners
 inputForm.addEventListener("submit", makeGrid);
-canvasTable.addEventListener("click", colorBackground);
