a411c79692c94a1e56ad7238b79d78b0efb07d6d
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-01-11 17:23:28 +0100

    first approach - creating html with js

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
 // Select color input
+let colorInput = document.getElementById("colorPicker").value;
+// color gets saved as hex value
+
 // Select size input
+let inputCols = document.getElementById("inputHeight").value; // i.e. 30
+let inputRows = document.getElementById("inputWidth").value; // i.e. 20
+// input gets saved as number
 
 // When size is submitted by the user, call makeGrid()
+let inputForm = document.getElementById("sizePicker");
+inputForm.addEventListener("submit", makeGrid);
+// not tested yet
 
 function makeGrid() {
+  let canvasTable = document.getElementById("pixelCanvas");
+  // later: set equal to result of loop that creates html
 
-// Your code goes here!
+  let rowElement = `<tr></tr>`;
+  let colElement = `<td></td>`;
 
+  for (let row = 1; row < inputRows; row++) {
+    rowElement += rowElement; // adds one row every time
+    for (let col = 1; col < inputCols; col++) {
+      colElement += colElement; // adds one col / cell every time
+    }
+  }
 }
