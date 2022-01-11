da122da2565b4d02c5dd35a6a9b9d656d379ed2d
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-01-11 18:41:51 +0100

    second approach - append tr and td children - works

--- designs.js
+++ designs.js
@@ -3,26 +3,30 @@ let colorInput = document.getElementById("colorPicker").value;
 // color gets saved as hex value
 
 // Select size input
-let inputCols = document.getElementById("inputHeight").value; // i.e. 30
-let inputRows = document.getElementById("inputWidth").value; // i.e. 20
-// input gets saved as number
+
+function makeGrid(event) {
+  event.preventDefault();
+  // getting access to input values and the table tag
+  let inputCols = document.getElementById("inputWidth").value;
+  let inputRows = document.getElementById("inputHeight").value;
+  let canvasTable = document.getElementById("pixelCanvas");
+
+  // when form is submitted all children get removed (if any)
+  while (canvasTable.firstChild) {
+    canvasTable.removeChild(canvasTable.firstChild);
+  }
+
+  // for-loop for building rows and cols
+  for (let row = 0; row < inputRows; row++) {
+    let rowElement = document.createElement("tr");
+    canvasTable.appendChild(rowElement);
+    for (let col = 0; col < inputCols; col++) {
+      let colElement = document.createElement("td");
+      rowElement.appendChild(colElement);
+    }
+  } // automatic return??
+}
 
 // When size is submitted by the user, call makeGrid()
 let inputForm = document.getElementById("sizePicker");
 inputForm.addEventListener("submit", makeGrid);
-// not tested yet
-
-function makeGrid() {
-  let canvasTable = document.getElementById("pixelCanvas");
-  // later: set equal to result of loop that creates html
-
-  let rowElement = `<tr></tr>`;
-  let colElement = `<td></td>`;
-
-  for (let row = 1; row < inputRows; row++) {
-    rowElement += rowElement; // adds one row every time
-    for (let col = 1; col < inputCols; col++) {
-      colElement += colElement; // adds one col / cell every time
-    }
-  }
-}
